aeb3964ad4c0b779f4f1a8ea97f40f31443f6531
Will Roberts <willroberts11@gmail.com>
2014-07-20 12:02:18 -0700

    Moves update.py code into utilities.py

--- pathofexile/utilities.py
+++ pathofexile/utilities.py
@@ -1,5 +1,6 @@
 import grequests
-import sys
+import os
+import pickle
 import time
 
 import pathofexile.api
@@ -90,3 +91,49 @@ def get_ladder_concurrently(ladder_id):
         time.sleep(0.5)  # 10 requests per second
 
     return entries
+
+
+def cache_ladder(league, force_update=False):
+    ''' Get all players from a ladder concurrently. Delays requests to avoid
+    being rate limited. Caches the response in a pickle file.
+
+    :param league: Name of the league
+    :param force_update: boolean to bypass caching/pickling
+    :return: dict object from API JSON response
+    '''
+    pickle_file = 'cache/%s' % (league.replace(' ', ''))
+    if os.path.isfile(pickle_file) and not force_update:
+        current_time = time.time()
+        pickle_time = os.path.getmtime(pickle_file)
+
+        # if the pickle file is less than one day old, return the pickle
+        if (current_time - pickle_time) < 86400:
+            with open(pickle_file, 'r') as f:
+                return pickle.load(f)
+
+    # otherwise, retrieve the ladder and then cache it locally
+    print 'Retrieving new ladder for \'%s\'. This can take up to a minute.' % (
+        league,
+    )
+    ladder = get_ladder_concurrently(league)
+    with open(pickle_file, 'w') as f:
+        pickle.dump(ladder, f)
+    return ladder
+
+
+def list_leagues(names_only=True, races_only=False):
+    ''' Returns up to 50 leagues.
+
+    :param names_only: Only include league names, as opposed to the entire
+        JSON structure of league metadata
+    :param races_only: Only include event leagues
+    :return: List of league names or data structures
+    '''
+    if races_only:
+        league_type = 'event'
+    else:
+        league_type = 'all'
+    leagues = pathofexile.api.list_leagues(league_type=league_type)
+    if names_only:
+        return [l.get('id') for l in leagues]
+    return leagues
