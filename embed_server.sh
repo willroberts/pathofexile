1ec77008482cfb87c60d1c9497108ed5bc814093
Will Roberts <will.roberts11@gmail.com>
2014-07-27 12:56:36 -0700

    Changes HTTP port

--- embed_server.sh
+++ embed_server.sh
@@ -4,5 +4,5 @@ source env/bin/activate || {
     echo 'Missing virtualenv in env/'
     exit 1
 }
-gunicorn -w 2 -b 0.0.0.0:8080 pathofexile.forum.embed_server:app &
-echo 'Done! App is running on http://0.0.0.0:8080'
+gunicorn -w 2 -b 0.0.0.0:80 pathofexile.forum.embed_server:app &
+echo 'Done! App is running on http://0.0.0.0:80'
