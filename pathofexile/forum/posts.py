88885cf3bd555dd21b0ba86a27e5a4bb8b768f17
Will Roberts <will.roberts11@gmail.com>
2014-07-28 00:16:26 -0700

    Uses get_html from forum.items

--- pathofexile/forum/posts.py
+++ pathofexile/forum/posts.py
@@ -3,21 +3,9 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 
+from items import get_html
+
 logging.basicConfig(filename='embed_server.log', level=logging.DEBUG)
-
-
-def get_html(shop_thread_id):
-    url = 'http://www.pathofexile.com/forum/view-thread/%s' % shop_thread_id
-    html = requests.get(url).content
-    html = html.replace(
-        '/favicon.ico',
-        'http://www.pathofexile.com/favicon.ico',
-    )
-    html = html.replace(
-        '/js/lib/modernizr',
-        'http://www.pathofexile.com/js/lib/modernizr',
-    )
-    return html
 
 
 class PostIsolator(object):
