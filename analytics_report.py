198c05dc6b96bce05dea81aad046c5effbb7d7d6
Will Roberts <will.roberts11@gmail.com>
2014-09-06 14:00:26 -0700

    Sets default leagues to avoid operating on an empty list in the case of a retrieval error

--- analytics_report.py
+++ analytics_report.py
@@ -3,8 +3,13 @@ import pathofexile.ladder.analytics
 
 
 def main():
-    leagues = [league.get('id') for league in pathofexile.api.get_leagues()] or\
-              ['Standard', 'Hardcore']
+    # set default leagues
+    leagues = ['Standard', 'Hardcore']
+    # try to get a list of updated leagues from the API
+    try:
+        leagues = [league.get('id') for league in pathofexile.api.get_leagues()]
+    except:
+        pass
     for league in leagues:
         pathofexile.ladder.analytics.report(league)
 
