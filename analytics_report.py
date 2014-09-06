6755689bb9c77bffcd2564e9e5ae64e448eaac7b
Paul Maciorowski <paul@pcm.io>
2014-09-06 16:14:23 -0400

    Analytics report now fetches a fresh league list if possible.

--- analytics_report.py
+++ analytics_report.py
@@ -1,8 +1,10 @@
+import pathofexile.api
 import pathofexile.ladder.analytics
 
 
 def main():
-    leagues = ['Standard', 'Hardcore', 'One Month Race', 'One Month Race HC']
+    leagues = [league.get('id') for league in pathofexile.api.get_leagues()] or\
+              ['Standard', 'Hardcore']
     for league in leagues:
         pathofexile.ladder.analytics.report(league)
 
